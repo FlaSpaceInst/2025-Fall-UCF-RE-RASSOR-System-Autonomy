86fd7f42539cb877af9d57997cbfa5b2f655a14f
Collin Pastika <cjpastika@gmail.com>
2026-02-09 22:28:11 -0500

    added tf tree code

---
+++ src/ezrassor_description/launch/robot_state_publisher.launch.py
@@ -0,0 +1,28 @@
+"""Launch robot_state_publisher with EZRASSOR URDF."""
+
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('ezrassor_description')
+    xacro_file = os.path.join(pkg_share, 'urdf', 'ezrassor.urdf.xacro')
+    robot_description_content = xacro.process_file(xacro_file).toxml()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description_content,
+            'use_sim_time': False,
+        }],
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+    ])
